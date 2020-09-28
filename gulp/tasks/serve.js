0d7c290de9787d7bc3b7ce3a158d7863340290d1
efekta <efekta9@gmail.com>
2020-09-28 22:01:43 +0300

    start template settings, svg images

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "cont.html"
+                index: "index.html"
             }
         });
     });
